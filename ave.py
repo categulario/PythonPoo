9b84b978aa91034299b5f4f63bb7b59e17bd3f0b
Abraham Toriz Cruz <abram_92@yahoo.com.mx>
2012-08-05 07:57:28 -0500

    Ahora si se ve como llamar al constructor de la clase padre

--- ave.py
+++ ave.py
@@ -1,34 +1,36 @@
 #experimentos con clases
 
 class Ave(object):
-	"""define un ave"""
-	def __init__(self, tamanio):
-		"""inicializa el ave"""
-		self.tamanio = tamanio
+    """define un ave"""
+    def __init__(self, tamanio):
+        """inicializa el ave"""
+        self.tamanio = tamanio
 
-	@staticmethod
-	def volar(distancia):
-		"""vuela un tramo"""
-		#funcion estatica
-		for i in xrange(distancia):
-			print "estoy volando", i
+    @staticmethod
+    def volar(distancia):
+        """vuela un tramo"""
+        #funcion estatica
+        for i in xrange(distancia):
+            print "estoy volando", i
 
 class Colibri(Ave):
-	def __init__(self, color):
-		"""inicializa un Colibri"""
-		#super.__init__(self, 'chico')
-		self.color = color
-		self.panza = 0
-		#super(Ave, self).__init__('chico')
+    def __init__(self, color):
+        """inicializa un Colibri"""
+        super(Colibri, self).__init__('chico')
+        #Asi se llama al constructor de la clave padre
+        #sin embargo se puede usar para llamar a cualquier metodo
+        #de la clase padre
+        self.color = color
+        self.panza = 0
 
-	def comer(self, comida):
-		#no es estatica
-		if comida:
-			self.panza += 10
+    def comer(self, comida):
+        #no es estatica
+        if comida:
+            self.panza += 10
 
 if __name__ == "__main__":
-	#metodo estatico
-	Colibri.volar(10)
-	#metodo no estatico
-	c = Colibri('verde')
-	c.comer(2)
+    #metodo estatico
+    Colibri.volar(10)
+    #metodo no estatico
+    c = Colibri('verde')
+    c.comer(2)
