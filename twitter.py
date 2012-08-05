319f231f4121a22a699eb1337bb95cd4fbd66b5d
Abraham Toriz Cruz <abram_92@yahoo.com.mx>
2012-08-05 07:59:44 -0500

    llamada a la API de twitter

--- twitter.py
+++ twitter.py
@@ -4,12 +4,11 @@ import urllib
 import json
 
 def buscarTweets(cadena):
-	busqueda = urllib.urlopen('http://search.twitter.com/search.json?q='+cadena)
-	diccionario = json.loads(busqueda.read())
-	for tweet in diccionario['results']:
-		print "*", tweet["text"]
+    busqueda = urllib.urlopen('http://search.twitter.com/search.json?q='+cadena)
+    diccionario = json.loads(busqueda.read())
+    for tweet in diccionario['results']:
+        print "----------"
+        print "*", tweet["text"]
 
 if __name__ == "__main__":
-	persona = {"nombre": "Pepe", "lenguajes": ['java', 'php', 'python']}
-	#print buscarTweets('python')
-	print persona['nombre']
+    print buscarTweets('python')
